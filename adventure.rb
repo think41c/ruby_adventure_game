1cf343c08e1a1873e9647fd0ffee3c0d93a77978
Tony DiNitto <think41c@gmail.com>
2015-08-20 23:39:48 -0400

    Validationf life input added.

--- adventure.rb
+++ adventure.rb
@@ -2,10 +2,18 @@ puts "Choose your own adventure!"
 puts "Begin with determining your hero's attributes"
 get_life
 def get_life
-	life = gets.chomp.to_i	
+	@life = gets.chomp.to_i	
+end
+validate_life
+
+def validate_life
+	if @life > 100
+		puts "Try again"
+		get_life
+	end
 end
 
 
-puts "You have #{rand(life)} points of life."
+puts "You have #{rand(@life)} points of life."
 
 
