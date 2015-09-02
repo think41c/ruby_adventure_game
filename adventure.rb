e51ed5ae524d5f5096f5956ce831c486accf5cdc
Tony DiNitto <think41c@gmail.com>
2015-09-01 23:15:24 -0400

    Moving over towards case statements.

--- adventure.rb
+++ adventure.rb
@@ -74,20 +74,20 @@ class Intro
     end
   end
 
-  def locale_01
+  def locale_00
     puts "You're outside a castle. You see a path to the north. N/S/E/W"    
     direction = gets.chomp
     if direction_validator(direction) == true
       puts cardinal_convert(direction)
     else
       "Choose again"
-      locale_01
+      locale_00
     end
 
     if direction.upcase == "N"
       puts "You kick your horse sternly to move Northward!"
-      location = "local_01"
-      local_01
+      location = "locale_01"
+      locale_01
     else 
       puts "You hit an impasse and return to the castle."
       locale_01
@@ -110,16 +110,22 @@ class Intro
     puts "You've picked an invalid direction"
   end
 
-  def local_01
+  def locale_01
     puts "You're standing in front of the castle. The path you came from is to the (S)outh. The gate is (N)orth."
     direction = gets.chomp
     
     if direction_validator(direction) == false 
       invalid_direction
-      local_01
+      locale_01
     end
 
     puts cardinal_convert(direction)
+    case direction 
+    when "N" 
+      # locale_02
+    when "S"
+      locale_00
+    end
 
     # May need to make a massive Case statement that 
     # asks what the local is and then switch to the 
